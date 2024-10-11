cb170f1a1dc0ffc1cc6af2780bafd898360169e0
Brandon <brandongarces1010@gmail.com>
2024-10-10 19:05:46 -0500

    modification list to only work with nodes

--- Huffmans_algorithm.py
+++ Huffmans_algorithm.py
@@ -1,14 +1,15 @@
 from tkinter import filedialog
 
 frequency = {}
+dic_encode = {}
 
 
 class Node:
     def __init__(self, value: int, letter: str = "", left=None, rigth=None):
         self.value = value
         self.letter = letter
-        self.next = left
-        self.back = rigth
+        self.left = left
+        self.rigth = rigth
 
     def __str__(self):
         return f"Node({self.value})"
@@ -44,44 +45,22 @@ def compress():
     # sorted list
     list_frequency = sorted(list_frequency, key=lambda x: x[1])
 
-    while len(list_frequency) > 1:
-        first = list_frequency.pop(0)
-        second = list_frequency.pop(0)
+    # Lista de nodos
+    list_nodes = []
+
+    for i in range(len(list_frequency)):
+        list_nodes.append(Node(list_frequency[i][1], list_frequency[i][0]))
+
+    while len(list_nodes) > 1:
+        first = list_nodes.pop(0)
+        second = list_nodes.pop(0)
 
         # print([str(obj) for obj in list_frequency])
+        node = Node(value=first.value + second.value, left=first, rigth=second)
 
-        if isinstance(first, tuple) and isinstance(second, tuple):
-            letter_f, value_f = first
-            letter_s, value_s = second
+        append_list(list_nodes, node)
 
-            node_f = Node(value=value_f, letter=letter_f)
-            node_s = Node(value=value_s, letter=letter_s)
-
-            node = Node(value=value_f + value_s, left=node_f, rigth=node_s)
-
-            append_list(list_frequency, node)
-        elif isinstance(first, tuple) and isinstance(second, Node):
-            letter_f, value_f = first
-
-            node_f = Node(value=value_f, letter=letter_f)
-
-            node = Node(value=value_f + second.value, left=node_f, rigth=second)
-
-            append_list(list_frequency, node)
-        elif isinstance(first, Node) and isinstance(second, tuple):
-            letter_s, value_s = second
-
-            node_s = Node(value=value_s, letter=letter_s)
-
-            node = Node(value=value_s + first.value, left=first, rigth=node_s)
-
-            append_list(list_frequency, node)
-        elif isinstance(first, Node) and isinstance(second, Node):
-            node = Node(value=first.value + second.value, left=first, rigth=second)
-
-            append_list(list_frequency, node)
-
-    print([str(obj) for obj in list_frequency])
+    print([str(obj) for obj in list_nodes])
 
 
 def append_list(list_huffman: list, node: Node):
@@ -89,34 +68,15 @@ def append_list(list_huffman: list, node: Node):
 
     for i in range(len(list_huffman) - 1):
 
-        if isinstance(list_huffman[i], tuple) and isinstance(
-            list_huffman[i + 1], tuple
-        ):
-            if list_huffman[i][1] <= node.value < list_huffman[i + 1][1]:
-                list_huffman.insert(i, node)
-                flag = True
-                break
-        elif isinstance(list_huffman[i], tuple) and isinstance(
-            list_huffman[i + 1], Node
-        ):
-            if list_huffman[i][1] <= node.value < list_huffman[i + 1].value:
-                list_huffman.insert(i, node)
-                flag = True
-                break
-        elif isinstance(list_huffman[i], Node) and isinstance(
-            list_huffman[i + 1], tuple
-        ):
-            if list_huffman[i].value <= node.value < list_huffman[i + 1][1]:
-                list_huffman.insert(i, node)
-                flag = True
-                break
-        elif isinstance(list_huffman[i], Node) and isinstance(
-            list_huffman[i + 1], Node
-        ):
-            if list_huffman[i].value <= node.value < list_huffman[i + 1].value:
-                list_huffman.insert(i, node)
-                flag = True
-                break
+        if list_huffman[i].value <= node.value < list_huffman[i + 1].value:
+            list_huffman.insert(i, node)
+            flag = True
+            break
 
     if not flag:
         list_huffman.append(node)
+
+
+def encrypt(encode: str, node: Node):
+    if node.rigth == None and node.left == None:
+        dic_encode = {node.letter, encode}
